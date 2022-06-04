a87e1d3dd7ef2064cd95a744781cd7f75a326bee
fiqih16 <fiqih1666@gmail.com>
2022-06-04 22:11:57 +0700

    New

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
+const names = ['Halo', 'Angel', 'Nyoman', 'Ketut', 'Aisyah'];
 
 // TODO: Fungsi utama
 // Menerima 2 parameter: list nama dan callback sort
@@ -6,20 +6,38 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, x) => {
+  const list = [];
+  let result = x(names);
+
+  for (let a = 0; a < result.length; a++) {
+    b = a + 1 + '. ' + result[a];
+    list.push(b);
+  }
+
+  return list;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  let c = names.sort((a, b) => (a > b ? 1 : -1));
+  console.log(c);
+  return c;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  let d = names.sort((a, b) => (a > b ? -1 : 1));
+  console.log(names);
+  return d;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
-  console.log(sorter?.(names, sortAscending)?.join("\n"));
-  console.log(sorter?.(names, sortDescending)?.join("\n"));
+  console.log(sorter?.(names, sortAscending)?.join('\n'));
+  console.log(sorter?.(names, sortDescending)?.join('\n'));
 })();
 
 module.exports = {
